e17f07fbb8878653856f89af9edcb8866418d09a
申潤五 <shenrunwu@shenrunwude-macbook-pro.local>
2017-01-13 17:05:26 +0800

    Initial Commit

---
+++ NLPA1Free/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NLPA1Free
+//
+//  Created by 申潤五 on 2017/1/13.
+//  Copyright © 2017年 申潤五. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
